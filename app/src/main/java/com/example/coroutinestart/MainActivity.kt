91ac6898927319c31a91453d330ea4df3fe0fc7d
Artem <artem.mamedov5375@gmail.com>
2024-01-06 15:28:00 +0200

    Structured Concurrency

--- app/src/main/java/com/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.example.coroutinestart
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.ViewModelProvider
 import com.example.coroutinestart.databinding.ActivityMainBinding
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,97 +11,14 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.buttonLoad.setOnClickListener {
-            binding.progress.isVisible = true
-            binding.buttonLoad.isEnabled = false
-            val deferredCity = lifecycleScope.async {
-                val city = loadCity()
-                city
-            }
-            val deferredTemp = lifecycleScope.async {
-                val temp = loadTemperature()
-                binding.tvTemperature.text = temp.toString()
-                temp
-            }
-            lifecycleScope.launch {
-                val city = deferredCity.await()
-                val temp = deferredTemp.await()
-                Toast.makeText(
-                    this@MainActivity,
-                    "City: $city Temp: $temp",
-                    Toast.LENGTH_SHORT
-                ).show()
-                binding.progress.isVisible = false
-                binding.buttonLoad.isEnabled = true
-            }
-//            loadDataWithoutCoroutine()
-        }
-    }
 
-    private suspend fun loadData() {
-        binding.progress.isVisible = true
-        binding.buttonLoad.isEnabled = false
-        val city = loadCity()
-
-        binding.tvLocation.text = city
-        val temp = loadTemperature()
-
-        binding.tvTemperature.text = temp.toString()
-        binding.progress.isVisible = false
-        binding.buttonLoad.isEnabled = true
-    }
-
-    private fun loadDataWithoutCoroutine(step: Int = 0, obj: Any? = null) {
-        when(step) {
-            0 -> {
-                binding.progress.isVisible = true
-                binding.buttonLoad.isEnabled = false
-                loadCityWithout {
-                    loadDataWithoutCoroutine(1, it)
-                }
-            }
-            1 -> {
-                val city = obj as String
-                binding.tvLocation.text = city
-                loadTemperatureWithout(city) {
-                    loadDataWithoutCoroutine(2, it)
-                }
-            }
-            2 -> {
-                val temp = obj as Int
-                binding.tvTemperature.text = temp.toString()
-                binding.progress.isVisible = false
-                binding.buttonLoad.isEnabled = true
-            }
-        }
-    }
-
-    private suspend fun loadCity(): String {
-        delay(2000)
-        return "Sumy"
-    }
-    private fun loadCityWithout(callback: (String) -> Unit) {
-        Handler(Looper.getMainLooper()).postDelayed({
-            callback.invoke("Sumy")
-        }, 5000)
-    }
-
-    private suspend fun loadTemperature(): Int {
-        delay(5000)
-        return 3
-    }
-    private fun loadTemperatureWithout(city: String, callback: (Int) -> Unit) {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            callback.invoke(17)
-        }, 5000)
+        viewModel.method()
     }
 }
